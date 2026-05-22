7a7cc00dc8ee4487f5d31f7e8f52b6041798452e
R4D <maduranga0117@gmail.com>
2026-05-23 00:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ menuToggle.addEventListener('click',()=>{
 navLinks.classList.toggle('active');
 });
 
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1');
+  sheet.appendRow([
+    new Date(), 
+    e.parameter.name, 
+    e.parameter.email, 
+    e.parameter.date, 
+    e.parameter.phone, 
+    e.parameter.guests, 
+    e.parameter.days, 
+    e.parameter.destinations, 
+    e.parameter.message
+  ]);
+  
+  return ContentService.createTextOutput("Success").setMimeType(ContentService.MimeType.TEXT);
+}
