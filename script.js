863795e8b13ea5bfddafaf5c9ad11cd42df870b8
R4D <maduranga0117@gmail.com>
2026-05-22 16:53:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,3 @@ menuToggle.addEventListener('click',()=>{
 navLinks.classList.toggle('active');
 });
 
-document.querySelector('.booking-form').addEventListener('submit',function(e){
-e.preventDefault();
-alert('Booking Request Sent Successfully!');
-});
-
-const form = document.getElementById('bookingForm');
-const successMsg = document.getElementById('successMessage');
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    
-    fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        body: formData
-    }).then(() => {
-        form.reset();
-        successMsg.style.display = 'block';
-        setTimeout(() => successMsg.style.display = 'none', 5000);
-    });
-});
